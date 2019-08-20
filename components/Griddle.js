eeb37720f125f50b56e42928ebc4e3bcaf9d6c5a
toumani <toumani49@gmail.com>
2019-08-20 10:40:31 +0100

    feature(game):add snake move right

--- components/Griddle.js
+++ components/Griddle.js
@@ -1,5 +1,3 @@
-import _ from 'lodash';
-
 import Box from './Box';
 
 class Griddle extends React.Component {
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,58 @@
+import _ from 'lodash';
+
 import Griddle from '../components/Griddle';
 
+const DIMENTION = 10;
+
 class Index extends React.Component {
+	constructor(props) {
+		super(props);
+
+		const head = {x: 6, y: 5, lead: null};
+		const neck = {x: 5, y: 5, lead: head};
+		const body = {x: 4, y: 5, lead: neck};
+		const tail = {x: 3, y: 5, lead: body};
+
+		this.state = {
+			presence: [
+				head,
+				neck,
+				body,
+				tail
+			],
+			head: head,
+			tail: tail
+		};
+	}
+
+	componentDidMount() {
+		setInterval(this.moveSnake, 3000);
+	}
+
+	moveSnake = () => {
+		const { head, tail, presence } = this.state;
+
+		let newPresence = presence;
+
+		/* Repositionning new head an tail */
+		let newHead = {x: head.x + 1, y: head.y, lead: null};
+		let newTail = tail.lead
+
+		newPresence.push(newHead);
+		_.remove(newPresence, {x: tail.x, y: tail.y})
+		
+		this.setState({
+			presence: newPresence,
+			head: newHead,
+			tail: newTail
+		});
+	}
+
 	render() {
-		const presence = [
-			{x: 0, y: 0},
-			{x: 2, y: 0},
-			{x: 9, y: 0},
-			{x: 8, y: 7},
-			{x: 0, y: 1}
-		];
 		return (
 			<div>
 				<h1>Hello next!</h1>
-				<Griddle presence={presence} />
+				<Griddle presence={this.state.presence} />
 			</div>
 		)
 	}
