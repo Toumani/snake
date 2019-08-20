cdeeec743c4e1e222dc4fc4dec66bd6e6574bbde
toumani <toumani49@gmail.com>
2019-08-20 11:09:58 +0100

    feature(game):add snake move in all directions

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import _ from 'lodash';
 import Griddle from '../components/Griddle';
 
 const DIMENTION = 10;
+const RIGHT = 'RIGHT';
+const LEFT = 'LEFT';
+const UP = 'UP';
+const DOWN = 'DOWN';
 
 class Index extends React.Component {
 	constructor(props) {
@@ -21,7 +25,8 @@ class Index extends React.Component {
 				tail
 			],
 			head: head,
-			tail: tail
+			tail: tail,
+			direction: RIGHT
 		};
 	}
 
@@ -30,15 +35,35 @@ class Index extends React.Component {
 	}
 
 	moveSnake = () => {
-		const { head, tail, presence } = this.state;
+		const { head, tail, presence, direction } = this.state;
 
 		let newPresence = presence;
 
 		/* Repositionning new head an tail */
-		let newHead = {x: head.x + 1, y: head.y, lead: null};
+		let newHead;
+		switch (direction) {
+			case RIGHT:
+				newHead = {x: head.x + 1, y: head.y, lead: null};
+				break;
+			case LEFT:
+				newHead = {x: head.x - 1, y: head.y, lead: null};
+				break;
+			case UP:
+				newHead = {x: head.x, y: head.y - 1, lead: null};
+				break;
+			case DOWN:
+				newHead = {x: head.x, y: head.y + 1, lead: null};
+				break;
+		}
+		
 		let newTail = tail.lead
 
 		newPresence.push(newHead);
+		_.forEach(newPresence, function(box) {
+			if (box.x === head.x && box.y === head.y) {
+				box.lead = newHead;
+			}
+		});
 		_.remove(newPresence, {x: tail.x, y: tail.y})
 		
 		this.setState({
@@ -48,11 +73,28 @@ class Index extends React.Component {
 		});
 	}
 
+	moveRight = () => {
+		this.setState({ direction: RIGHT });
+	}
+	moveLeft = () => {
+		this.setState({ direction: LEFT });
+	}
+	moveUp = () => {
+		this.setState({ direction: UP });
+	}
+	moveDown = () => {
+		this.setState({ direction: DOWN });
+	}
+
 	render() {
 		return (
 			<div>
 				<h1>Hello next!</h1>
 				<Griddle presence={this.state.presence} />
+				<button onClick={this.moveRight}>Right</button>
+				<button onClick={this.moveLeft}>Left</button>
+				<button onClick={this.moveUp}>Up</button>
+				<button onClick={this.moveDown}>Down</button>
 			</div>
 		)
 	}
